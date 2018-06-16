04b4068d1b89b2d1c74a51602f312f99d1e83daa
lahiruhashan <hashanlahiru6@gmail.com>
2018-06-17 00:48:45 +0530

    other backend commits

---
+++ routes/users.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+const User = require('../models/user');
+
+//register
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password,
+        batch: req.body.batch,
+        indexNumber: req.body.indexNumber
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err){
+            res.json({
+                success : false,
+                msg : 'Failed to register user'
+            });
+        }else{
+            res.json({
+                success : true,
+                msg : 'User Registered'
+            });
+        }
+    });
+});
+
+//authenticate
+router.post('/authenticate', (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.getUserByEmail(email, (err, user) => {
+        if (err){
+            throw err;
+        }
+        if (!user) {
+            return res.json({
+                success : false,
+                msg : "User not found"
+            });
+        }
+
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if (err){
+                throw err;
+            }
+            if (isMatch){
+                const token = jwt.sign({data : user}, config.secret, {
+                    expiresIn: 604800 //1 week
+                });
+
+                res.json({
+                    success : true,
+                    token : `Bearer ${token}`,
+                    user : {
+                        id : user._id,
+                        name : user.name,
+                        email : user.email,
+                        batch: user.batch,
+                        indexNumber: user.indexNumber
+                    }
+                });
+            }else{
+                return res.json({
+                    success : false,
+                    msg : 'Wrong password'
+                });
+            }
+        });
+    });
+});
+
+//profile
+router.get('/profile', passport.authenticate('jwt', {session : false}), (req, res, next) => {
+    res.json({
+        user : req.user
+    })
+});
+
+module.exports = router;
