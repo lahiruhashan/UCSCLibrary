00d3672b58008fc031485387e1f1f16f07828395
lahiruhashan <hashanlahiru6@gmail.com>
2018-06-17 00:47:31 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+    console.log("Connected to database " + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log("Database error " + err);
+});
+
+const app = express();
+
+const port = 3000;
+
+const users = require('./routes/users');
+
+// CORS middleware
+app.use(cors());
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser middleware
+app.use(bodyParser.json());
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.send("Invalid Endpoint");
+});
+
+app.listen(3000, () => {
+    console.log("Server Started On Port " + port);
+});
